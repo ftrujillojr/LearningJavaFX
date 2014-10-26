9f9ef5d1f0a5698198de7e43b8ad6ca71bcd303e
Francis Trujillo <ftrujillojr@gmail.com>
2014-10-26 09:47:37 -0600

    added chkbox

--- src/org/trujillo/francis/fx/LoginController.java
+++ src/org/trujillo/francis/fx/LoginController.java
@@ -13,6 +13,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 
@@ -22,11 +23,13 @@ import javafx.scene.control.MenuBar;
  * @author ftrujillo
  */
 public class LoginController implements Initializable {
-    
+
     @FXML
-    private MenuBar menuLogin;
+    private MenuBar menuBarLogin;
     @FXML
     private Button btnExit;
+    @FXML
+    private CheckBox chbxDebug;
 
     /**
      *
@@ -37,21 +40,30 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        assert menuLogin != null : "fx:id=\"menuLogin\" was not injected: check your FXML file 'Login.fxml'.";
+        assert menuBarLogin != null : "fx:id=\"menuBarLogin\" was not injected: check your FXML file 'Login.fxml'.";
         assert btnExit != null : "fx:id=\"btnExit\" was not injected: check your FXML file 'Login.fxml'.";
-        
-        ListIterator<Menu> menusItr = menuLogin.getMenus().listIterator();
-        
+        assert chbxDebug != null : "fx:id=\"chbxDebug\" was not injected: check your FXML file 'Login.fxml'.";
+
+        ListIterator<Menu> menusItr = menuBarLogin.getMenus().listIterator();
+
         while (menusItr.hasNext()) {
             Menu menu = menusItr.next();
         }
-        
+
         btnExit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("btnExit  Event Handler pressed.");
             }
         });
-        
+
+        chbxDebug.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                boolean selected = chbxDebug.isSelected();
+                System.out.println("CheckBox Action (selected: " + selected + ")");
+            }
+        });
+
     }
 }
